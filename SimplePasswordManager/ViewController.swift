ed7f27e90b726f902a9cfe509269a81a14568231
junpei ono <snorlax.chemist.and.jazz@gmail.com>
2019-04-21 00:40:12 +0900

    create project

---
+++ SimplePasswordManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimplePasswordManager
+//
+//  Created by junpei ono on 2019/04/21.
+//  Copyright © 2019 com.swiswiswift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
